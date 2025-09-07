b82cf8f433843936a20a808c265667098aa312cb
Alberto L. Bonfiglio <AlbertoBonfiglio@users.noreply.github.com>
2025-09-07 17:51:00 +1000

    Update selector.sh

--- ui.d/selector.sh
+++ ui.d/selector.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-workflow=$(printf "node\ndotnet" | fzf --height 40% --border --prompt="🔧 Select workflow: ")
+workflow=$(printf "node\ndotnet\nrust" | fzf --height 40% --border --prompt="🔧 Select workflow: ")
